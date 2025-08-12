146b376d589c6405c757d0b28156e8b160b52ceb
Tim-Lukas Blom <timlukasblom@gmail.com>
2025-08-12 21:28:50 +0200

    moved back
    Signed-off-by: Tim-Lukas Blom <timlukasblom@gmail.com>

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -14,8 +14,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_A  , KC_R    , KC_S   , KC_T  , KC_G  ,                         KC_M  , KC_N  , KC_E  , KC_I  , KC_O    , KC_QUOT,
         KC_LCTL, KC_Z  , KC_X    , KC_C   , KC_D  , KC_V  ,                         KC_K  , KC_H  , KC_COMM, KC_DOT , KC_SLSH, KC_RCTL,
                          KC_LBRC , KC_LGUI,                                                       KC_RGUI, KC_EQL,
-                                         LOWER  , KC_SPC ,                          KC_ENT,   UPPER,
-                                         KC_LGUI, KC_LSFT,                         KC_BSPC,  KC_RSFT,
+                                         LOWER  , KC_SPC ,                          KC_ENT,   KC_BSPC,
+                                         KC_LGUI, KC_LSFT,                           UPPER,  KC_RSFT,
                                          KC_LCTL, KC_LALT,                         KC_RALT,   KC_RCTL
     ),
 
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
           KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6   , KC_7  , KC_8  , KC_9  ,   KC_0,_______,
           _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,KC_VOLU,
-          _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
+          _______,_______,_______,_______,_______,_______,                        _______,KC_PGUP,KC_PGDN,_______,_______,KC_VOLD,
                                           _______,_______,                            KC_EQL ,_______,
                                                   _______,_______,            _______,_______,
                                                   _______,_______,            _______,_______,
