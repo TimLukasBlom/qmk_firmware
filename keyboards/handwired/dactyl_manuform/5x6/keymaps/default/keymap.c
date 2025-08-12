0abc19e3326f10478383d2aedf277d07e476e14a
Tim-Lukas Blom <timlukasblom@gmail.com>
2025-08-12 21:03:08 +0200

    dactyl moved backspace
    Signed-off-by: Tim-Lukas Blom <timlukasblom@gmail.com>

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -14,8 +14,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_A  , KC_R    , KC_S   , KC_T  , KC_G  ,                         KC_M  , KC_N  , KC_E  , KC_I  , KC_O    , KC_QUOT,
         KC_LCTL, KC_Z  , KC_X    , KC_C   , KC_D  , KC_V  ,                         KC_K  , KC_H  , KC_COMM, KC_DOT , KC_SLSH, KC_RCTL,
                          KC_LBRC , KC_LGUI,                                                       KC_RGUI, KC_EQL,
-                                         LOWER  , KC_SPC ,                          KC_ENT,   KC_BSPC,
-                                         _______, KC_LSFT,                         KC_BSPC,  KC_RSFT,
+                                         LOWER  , KC_SPC ,                          KC_ENT,   UPPER,
+                                         KC_LGUI, KC_LSFT,                         KC_BSPC,  KC_RSFT,
                                          KC_LCTL, KC_LALT,                         KC_RALT,   KC_RCTL
     ),
 
